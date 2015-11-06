684152855fe9988c8483548cb302d6d5c05f50e0
isabel cusick <cusick.isabel@gmail.com>
2015-11-06 10:55:28 -0500

    changed style. working on generating a shuffled array.

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ var $topRight;
 var $bottomLeft;
 var $bottomRight;
 var $playerArray = [];
-var $computerArray = [$topLeft, $topRight, $bottomLeft, $bottomRight];
+var $colorArray = [$topLeft, $topRight, $bottomLeft, $bottomRight];
+var $computerArray;
 
 var flash = function($event) {
 	var flashIt = function() {
@@ -16,12 +17,28 @@ var flash = function($event) {
 	return flashIt;
 };
 
+var flashComputer = function($event) {
+	var flashTheComputer = function() {
+		// lines 23-25 need to happen to each element in computerArray. computerArray needs to be an array of randomly selected elements from colorArray
+		$computerArray = colorArray.map(function(current, index)
+			$colorArray.length += 1;
+			index = Math.floor(Math.random() * 5)
+			)
+		$event.css('visibility', 'hidden');
+		
+		setTimeout(function() { $event.css('visibility', 'visible') }, 200);
+	};
+	return flashIt;
+};
+// computerArray needs to randomly select a certain number of elements from  
+
 $(document).ready(function() {
 
 $topLeft = $('.top-left');
 $topRight = $('.top-right');
 $bottomLeft = $('.bottom-left');
 $bottomRight = $('.bottom-right');
+$computerColor = 
 
 
 $topLeft.on('click', flash($topLeft));
